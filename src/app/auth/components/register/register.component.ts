8ae9627c9e6098b47604c0940b51df77b7d45bd7
Hvitrevs <venusof12pm@proton.me>
2023-11-28 15:23:05 -0500

    webpack config issue resolve tmrw

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,11 +1,14 @@
 import { Component } from "@angular/core";
 import { FormBuilder, ReactiveFormsModule, Validators } from "@angular/forms";
+import { Store } from "@ngrx/store";
+import { register } from "../../store/actions";
+
 
 @Component({
   selector: 'mc-register',
   templateUrl: './register.component.html',
   standalone: true,
-  imports: [ReactiveFormsModule]
+  imports: [ReactiveFormsModule],
 
 })
 export class RegisterComponent {
@@ -14,11 +17,11 @@ export class RegisterComponent {
     email: ['', Validators.required],
     password: ['', Validators.required],
   })
-  constructor(private fb: FormBuilder){}
+  constructor(private fb: FormBuilder, private store: Store){}
   
   onSubmit(){
     console.log('form', this.form.getRawValue())
-    this.registerService(this.form.getRawValue()).subscribe()
+    this.store.dispatch(register())
   }
 }
 
